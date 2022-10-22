cf17736a6b032701c270c88770826b3af2fb0f7c
Cleudiney Brandão <cleudiney@hotmail.com>
2022-10-22 18:29:26 -0300

    Adicionados funcoes de distancia funcao principal chamada robotica.ino

---
+++ robotica.ino
@@ -0,0 +1,43 @@
+/* Sensor Ultrassônico HC-SR04
+
+// Definir os números dos pinos
+const int trigger = 8;
+const int eco     = 7;
+const int buzzer  = 4;
+const int led     = 3;
+
+// Definir as variáveis
+     long  duracao;
+     float dist;
+
+void setup() 
+{
+ pinMode(trigger, OUTPUT); // Configura o pino trigger como saída
+ pinMode(eco, INPUT); // Configura o pino eco como entrada.
+ pinMode(buzzer, OUTPUT); // Configura o pino de acionamento do buzzer
+ pinMode(led, OUTPUT); // Configura o pino de acionamento do LED
+ digitalWrite(led, LOW); // Desliga LED ao iniciar a placa.
+ digitalWrite(buzzer, LOW); // Desliga buzzer ao iniciar a placa.
+}
+*/
+
+//insercao das bibliotecas
+
+//configuracao dos escopos de funcao
+
+//configuracao dos defines
+
+//configuracao das constantes
+
+//configuracao das variaveis globais
+
+//setup do controlador
+void setup()
+{
+
+}
+//loop do controlador
+void loop()
+{
+    
+}
